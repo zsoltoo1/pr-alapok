61982ce64b3c16d55179261c3a38f7be956ff667
zsoltoo1 <133741148+zsoltoo1@users.noreply.github.com>
2023-09-21 10:09:22 +0200

    Add files via upload

---
+++ 3-4.ora/feladat_003.py
@@ -0,0 +1,9 @@
+# feladat_003
+# dk9 95-oldal
+print(f'Üdv néked!')
+évek_száma = input('Hány éves vagy? ')
+évek_száma = int(évek_száma)
+if évek_száma < 14:
+ print(f"Általános iskolába jársz.")
+else:
+ print(f"Egy év múlva {évek_száma+1} éves leszel.")
---
+++ 3-4.ora/feladat_004.py
@@ -0,0 +1,8 @@
+# feladat_004
+IDEI_ÉV = 2023
+felhasználó_kora = input('Hány éves vagy? ')
+print(f"Te most {felhasználó_kora} éves vagy.")
+felhasználó_kora = int(felhasználó_kora)
+születési_év = IDEI_ÉV - felhasználó_kora
+print(f"Ekkor születtél: {születési_év}.")
+
