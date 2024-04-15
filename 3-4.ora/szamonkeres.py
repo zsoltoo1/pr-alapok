78a4db88d36b3c60ae0ac1f2564b913f478c0f7a
zsoltoo1 <133741148+zsoltoo1@users.noreply.github.com>
2024-04-15 16:49:48 +0200

    Delete 3-4.ora directory

--- 3-4.ora/szamonkeres.py
+++
@@ -1,9 +0,0 @@
-# Dudás Zsolt 10.d
-# szamonkeres001.py
-"""
-Bekérek egy osztályzatot 1-5 irassuk ki a megadott jegyet értékkel és szövegesen, ha nem megfelelő jegyet adtam akkor irja ki a jegy értékét és mellé azt a szöveget hogy nem megfelelő
-"""
-jegy = int(input("Adj meg egy jegyet:" ))
-if jegy:1
-print(f"Az osztályzatod elégtelen.")
-else jegy:2
